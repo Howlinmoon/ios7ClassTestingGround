328b5c824afe0523175605468a18039f74b3edbf
Jim Veneskey <github@strifecrafter.com>
2015-03-11 12:47:06 -0400

    Initial Commit

---
+++ Class Testing Ground/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Class Testing Ground
+//
+//  Created by jim Veneskey on 3/11/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Class Testing Ground/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Class Testing Ground
+//
+//  Created by jim Veneskey on 3/11/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
